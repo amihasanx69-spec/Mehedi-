052bb2af173a7a93378d21466ed984fde20d6dfc
amihasanx69-spec <amihasanx69@gmail.com>
2026-05-10 13:21:38 +0600

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -1,36 +1,61 @@
 const fs = require('fs');
 
 module.exports = {
+
 	config: {
-		name: "file",
-		aliases: ["files", "sendfile"],
+
+		name: "file2",
+
+		aliases: ["files","f"," fi",],
+
 		version: "1.0",
-		author: "404",
+
+		author: "SEXY ROCKY",
+
 		countDown: 5,
+
 		role: 0,
+
 		shortDescription: "Send bot script",
+
 		longDescription: "Send bot specified file ",
+
 		category: "𝗢𝗪𝗡𝗘𝗥",
+
 		guide: "{pn} file name. Ex: .{pn} filename"
+
 	},
 
 	onStart: async function ({ message, args, api, event }) {
-		const permission = ["100045644423035"];
+
+		const permission = ["61588972996269","61580336378735", "61586144220686"];
+
 		if (!permission.includes(event.senderID)) {
-			return api.sendMessage("📛 You have no permission this cmd only  used this cmd..", event.threadID, event.messageID);
+
+			return api.sendMessage(" 🫢🌺ভাগ মাগি আমার বস হাসান ছারা তোর নানাও পারবেনা কমান্ড চুরি করতে. 😝🤣🫦😩", event.threadID, event.messageID);
+
 		}
 
 		const fileName = args[0];
+
 		if (!fileName) {
+
 			return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+
 		}
 
 		const filePath = __dirname + `/${fileName}.js`;
+
 		if (!fs.existsSync(filePath)) {
+
 			return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+
 		}
 
 		const fileContent = fs.readFileSync(filePath, 'utf8');
+
 		api.sendMessage({ body: fileContent }, event.threadID);
+
 	}
+
 };
