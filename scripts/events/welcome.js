8cd2f9784a75d82069d1fdca24983b2588c68cd4
amihasanx69-spec <amihasanx69@gmail.com>
2026-05-12 02:09:56 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -1,132 +1,198 @@
-const { getTime, drive } = global.utils;
-if (!global.temp.welcomeEvent)
-	global.temp.welcomeEvent = {};
+const { drive } = global.utils;
+const { nickNameBot } = global.GoatBot.config;
+const { createCanvas, loadImage } = require("canvas");
+const fs = require("fs-extra");
+const path = require("path");
+const axios = require("axios");
 
 module.exports = {
-	config: {
-		name: "welcome",
-		version: "1.7",
-		author: "NTKhang",
-		category: "events"
-	},
+  config: {
+    name: "welcome",
+    version: "8.0",
+    author: "EryXenX",
+    category: "events"
+  },
 
-	langs: {
-		vi: {
-			session1: "sáng",
-			session2: "trưa",
-			session3: "chiều",
-			session4: "tối",
-			welcomeMessage: "Cảm ơn bạn đã mời tôi vào nhóm!\nPrefix bot: %1\nĐể xem danh sách lệnh hãy nhập: %1help",
-			multiple1: "bạn",
-			multiple2: "các bạn",
-			defaultWelcomeMessage: "Xin chào {userName}.\nChào mừng bạn đến với {boxName}.\nChúc bạn có buổi {session} vui vẻ!"
-		},
-		en: {
-			session1: "morning",
-			session2: "noon",
-			session3: "afternoon",
-			session4: "evening",
-			welcomeMessage: "Thank you for inviting me to the group!\nBot prefix: %1\nTo view the list of commands, please enter: %1help",
-			multiple1: "you",
-			multiple2: "you guys",
-			defaultWelcomeMessage: `Hello {userName}.\nWelcome {multiple} to the chat group: {boxName}\nHave a nice {session} 😊`
-		}
-	},
+  langs: {
+    en: {
+      defaultWelcomeMessage: "𝗪𝗲𝗹𝗰𝗼𝗺𝗲 {userName} 🎉\n┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄\n✦ Glad to have you here! Enjoy your stay and make great memories 🌸",
+      botAddedMessage:
+        "━━━━━━━━━━━━━━━━━━━\n🤖 ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ᴛᴏ ᴛʜᴇ ɢʀᴏᴜᴘ! 💖\n\n⚙️ ʙᴏᴛ ᴘʀᴇꜰɪx : /\n📜 ᴛʏᴘᴇ /help ᴛᴏ sᴇᴇ ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs\n\n✨ ʟᴇᴛ's ᴍᴀᴋᴇ ᴛʜɪs ɢʀᴏᴜᴘ ᴇᴠᴇɴ ᴍᴏʀᴇ ꜰᴜɴ ᴛᴏɢᴇᴛʜᴇʀ! 😄\n━━━━━━━━━━━━━━━━━━━"
+    }
+  },
 
-	onStart: async ({ threadsData, message, event, api, getLang }) => {
-		if (event.logMessageType == "log:subscribe")
-			return async function () {
-				const hours = getTime("HH");
-				const { threadID } = event;
-				const { nickNameBot } = global.GoatBot.config;
-				const prefix = global.utils.getPrefix(threadID);
-				const dataAddedParticipants = event.logMessageData.addedParticipants;
-				// if new member is bot
-				if (dataAddedParticipants.some((item) => item.userFbId == api.getCurrentUserID())) {
-					if (nickNameBot)
-						api.changeNickname(nickNameBot, threadID, api.getCurrentUserID());
-					return message.send(getLang("welcomeMessage", prefix));
-				}
-				// if new member:
-				if (!global.temp.welcomeEvent[threadID])
-					global.temp.welcomeEvent[threadID] = {
-						joinTimeout: null,
-						dataAddedParticipants: []
-					};
+  onStart: async ({ threadsData, message, event, api, usersData, getLang }) => {
+    if (event.logMessageType !== "log:subscribe") return;
 
-				// push new member to array
-				global.temp.welcomeEvent[threadID].dataAddedParticipants.push(...dataAddedParticipants);
-				// if timeout is set, clear it
-				clearTimeout(global.temp.welcomeEvent[threadID].joinTimeout);
+    const { threadID } = event;
+    const threadData = await threadsData.get(threadID);
+    if (!threadData.settings.sendWelcomeMessage) return;
 
-				// set new timeout
-				global.temp.welcomeEvent[threadID].joinTimeout = setTimeout(async function () {
-					const threadData = await threadsData.get(threadID);
-					if (threadData.settings.sendWelcomeMessage == false)
-						return;
-					const dataAddedParticipants = global.temp.welcomeEvent[threadID].dataAddedParticipants;
-					const dataBanned = threadData.data.banned_ban || [];
-					const threadName = threadData.threadName;
-					const userName = [],
-						mentions = [];
-					let multiple = false;
+    const addedMembers = event.logMessageData.addedParticipants;
+    const threadName   = threadData.threadName || "our group";
+    const prefix       = global.utils.getPrefix(threadID);
+    const inviterID    = event.author;
 
-					if (dataAddedParticipants.length > 1)
-						multiple = true;
+    for (const user of addedMembers) {
+      const userID = user.userFbId;
+      const botID  = api.getCurrentUserID();
 
-					for (const user of dataAddedParticipants) {
-						if (dataBanned.some((item) => item.id == user.userFbId))
-							continue;
-						userName.push(user.fullName);
-						mentions.push({
-							tag: user.fullName,
-							id: user.userFbId
-						});
-					}
-					// {userName}:   name of new member
-					// {multiple}:
-					// {boxName}:    name of group
-					// {threadName}: name of group
-					// {session}:    session of day
-					if (userName.length == 0) return;
-					let { welcomeMessage = getLang("defaultWelcomeMessage") } =
-						threadData.data;
-					const form = {
-						mentions: welcomeMessage.match(/\{userNameTag\}/g) ? mentions : null
-					};
-					welcomeMessage = welcomeMessage
-						.replace(/\{userName\}|\{userNameTag\}/g, userName.join(", "))
-						.replace(/\{boxName\}|\{threadName\}/g, threadName)
-						.replace(
-							/\{multiple\}/g,
-							multiple ? getLang("multiple2") : getLang("multiple1")
-						)
-						.replace(
-							/\{session\}/g,
-							hours <= 10
-								? getLang("session1")
-								: hours <= 12
-									? getLang("session2")
-									: hours <= 18
-										? getLang("session3")
-										: getLang("session4")
-						);
+      if (userID == botID) {
+        if (nickNameBot) await api.changeNickname(nickNameBot, threadID, botID);
+        return message.send(getLang("botAddedMessage", prefix));
+      }
 
-					form.body = welcomeMessage;
+      const userName    = user.fullName;
+      const inviterName = await usersData.getName(inviterID);
+      const memberCount = event.participantIDs.length;
 
-					if (threadData.data.welcomeAttachment) {
-						const files = threadData.data.welcomeAttachment;
-						const attachments = files.reduce((acc, file) => {
-							acc.push(drive.getFile(file, "stream"));
-							return acc;
-						}, []);
-						form.attachment = (await Promise.allSettled(attachments))
-							.filter(({ status }) => status == "fulfilled")
-							.map(({ value }) => value);
-					}
-					message.send(form);
-					delete global.temp.welcomeEvent[threadID];
-				}, 1500);
-			};
-	}
+      let { welcomeMessage = getLang("defaultWelcomeMessage") } = threadData.data;
+      welcomeMessage = welcomeMessage
+        .replace(/\{userName\}/g, userName)
+        .replace(/\{userTag\}/g, userName)
+        .replace(/\{threadName\}/g, threadName)
+        .replace(/\{memberCount\}/g, memberCount)
+        .replace(/\{inviterName\}/g, inviterName);
+
+      let welcomeImagePath = null;
+      try {
+        welcomeImagePath = await createWelcomeCard({
+          userName, threadName, memberCount,
+          inviterName, newUserID: userID,
+          inviterID, threadID, api
+        });
+      } catch (err) {
+        console.error("Welcome image creation failed:", err);
+      }
+
+      const form = {
+        body: welcomeMessage,
+        mentions: [{ tag: userName, id: userID }]
+      };
+
+      if (welcomeImagePath && fs.existsSync(welcomeImagePath)) {
+        form.attachment = fs.createReadStream(welcomeImagePath);
+      } else if (threadData.data.welcomeAttachment) {
+        const attachments = threadData.data.welcomeAttachment
+          .map(f => drive.getFile(f, "stream"));
+        form.attachment = (await Promise.allSettled(attachments))
+          .filter(({ status }) => status === "fulfilled")
+          .map(({ value }) => value);
+      }
+
+      message.send(form);
+
+      if (welcomeImagePath && fs.existsSync(welcomeImagePath)) {
+        setTimeout(() => { try { fs.unlinkSync(welcomeImagePath); } catch (_) {} }, 5000);
+      }
+    }
+  }
 };
+
+const ACCESS_TOKEN = "6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
+
+async function downloadHighQualityProfile(userID) {
+  try {
+    const url = `https://graph.facebook.com/${userID}/picture?width=500&height=500&access_token=${ACCESS_TOKEN}`;
+    const res = await axios({ method: 'GET', url, responseType: 'arraybuffer', timeout: 10000 });
+    return Buffer.from(res.data, 'binary');
+  } catch { return null; }
+}
+
+async function downloadImage(url) {
+  try {
+    const res = await axios({ method: 'GET', url, responseType: 'arraybuffer', timeout: 10000 });
+    return Buffer.from(res.data, 'binary');
+  } catch { return null; }
+}
+
+async function getGroupImage(threadID, api) {
+  try {
+    const info = await api.getThreadInfo(threadID);
+    if (info.imageSrc) {
+      const res = await axios({ method: 'GET', url: info.imageSrc, responseType: 'arraybuffer', timeout: 10000 });
+      return Buffer.from(res.data, 'binary');
+    }
+  } catch {}
+  return null;
+}
+
+function unicodeToPlain(str) {
+  if (!str) return '';
+  const ranges = [
+    [0x1D400, 0x1D419, 'A'], [0x1D41A, 0x1D433, 'a'],
+    [0x1D434, 0x1D44D, 'A'], [0x1D44E, 0x1D467, 'a']
+  ];
+
+  let result = '';
+  for (const char of str) result += char;
+  return result;
+}
+
+function safeStr(str) {
+  if (!str) return '';
+  try { return Buffer.from(str, 'latin1').toString('utf8'); } catch { return str; }
+}
+
+function readableText(str) {
+  return safeStr(str);
+}
+
+function ordinal(n) {
+  const s = ['th', 'st', 'nd', 'rd'], v = n % 100;
+  return n + (s[(v - 20) % 10] || s[v] || s[0]);
+}
+
+function roundRect(ctx, x, y, w, h, r) {
+  ctx.beginPath();
+  ctx.moveTo(x + r, y);
+  ctx.lineTo(x + w - r, y);
+  ctx.quadraticCurveTo(x + w, y, x + w, y + r);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+  ctx.lineTo(x + r, y + h);
+  ctx.quadraticCurveTo(x, y + h, x, y + h - r);
+  ctx.lineTo(x, y + r);
+  ctx.quadraticCurveTo(x, y, x + r, y);
+  ctx.closePath();
+}
+
+function drawCircleAvatar(ctx, img, cx, cy, r) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(cx, cy, r, 0, Math.PI * 2);
+  ctx.clip();
+  ctx.drawImage(img, cx - r, cy - r, r * 2, r * 2);
+  ctx.restore();
+}
+
+function fitText(ctx, text, maxPx, maxSize = 34, minSize = 14) {
+  let size = maxSize;
+  ctx.font = `${size}px Arial`;
+  while (ctx.measureText(text).width > maxPx && size > minSize) {
+    size--;
+    ctx.font = `${size}px Arial`;
+  }
+  return { text, size };
+}
+
+async function createWelcomeCard(data) {
+  const W = 1200, H = 630;
+  const canvas = createCanvas(W, H);
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "#0d0d16";
+  ctx.fillRect(0, 0, W, H);
+
+  ctx.fillStyle = "white";
+  ctx.font = "bold 30px Arial";
+  ctx.textAlign = "center";
+  ctx.fillText("Welcome Card", W / 2, H / 2);
+
+  ctx.font = "20px Arial";
+  ctx.fillText("Powered By Hasan", W / 2, H / 2 + 60);
+
+  const file = path.join(__dirname, `welcome_${Date.now()}.png`);
+  fs.writeFileSync(file, canvas.toBuffer("image/png"));
+  return file;
+}
